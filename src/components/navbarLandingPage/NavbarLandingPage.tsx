850a517df912a766ab0cb8f649e426a1f0c863f9
Franco Diaz <dfrancoandres@gmail.com>
2023-11-23 13:05:23 -0300

    se agrega navlink de react router dom para manejar el navbar y su CSS

--- src/components/navbarLandingPage/NavbarLandingPage.tsx
+++ src/components/navbarLandingPage/NavbarLandingPage.tsx
@@ -1,23 +1,41 @@
 import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 import './navbarlandingpage.css';
-import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import useAuthStore from '../../store/auth.store';
 
 //Components
 
 function NavbarLandingPage() {
-  const language = useAuthStore(state => state.language)
   const setLanguage = useAuthStore((state) => state.setLanguage);
+  const language = useAuthStore(state => state.language);
+
   const handleChangeLanguage = () => {
     setLanguage(!language)
   }
+
   return (
     <nav className='navbarLandingPageMainContainer'>
       <ul className='optionsNavbarContainer'>
-        <li className='optionsNavbar'>Home</li>
-        <li className='optionsNavbar'>Skills</li>
-        <li className='optionsNavbar'>Projects</li>
-        <li className='optionsNavbar'>Personal Blog</li>
+        <li>
+          <NavLink to='/' className={({isActive})=> isActive ? 'optionsNavbar activeLink' : 'optionsNavbar'}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/skills' className={({isActive})=> isActive ? 'optionsNavbar activeLink' : 'optionsNavbar'}>
+            Skills
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/projects' className={({isActive})=> isActive ? 'optionsNavbar activeLink' : 'optionsNavbar'}>
+            Projects
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/blog' className={({isActive})=> isActive ? 'optionsNavbar activeLink' : 'optionsNavbar'}>
+            Blog
+          </NavLink>
+        </li>
       </ul>
       <ul className='socialMediaIconsContainer'>
         <span className="material-symbols-outlined" onClick={handleChangeLanguage}>
